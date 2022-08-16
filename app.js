975484865223115e8ba0ff74fc32cbf7e5b2f5d8
Seth DImmer <sethdimmer1@gmail.com>
2022-08-16 04:22:11 -0400

    initial commit

---
+++ app.js
@@ -0,0 +1,23 @@
+// Question 1. Falsy or Truthy?
+
+// Given two values, return the first one if it is falsy, otherwise return the seconds one.
+
+// filterOutFalsy(0, 500) --> 0
+
+// filterOutFalsy(false, 100) --> false
+
+// filterOutFalsy([true, 'Dog']) --> 'Dog'
+
+
+
+
+
+
+
+
+
+
+
+
+
+
