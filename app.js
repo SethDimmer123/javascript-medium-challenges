0a13b5d44b2aa7dbc42964bbc69fa364f4797a94
Seth DImmer <sethdimmer1@gmail.com>
2022-11-15 02:35:21 -0500

    initial commit

--- app.js
+++ app.js
@@ -9,12 +9,19 @@
 // filterOutFalsy([true, 'Dog']) --> 'Dog'
 
 
+function filterOutFalsy(num1,num2) {
+    if(num1){
+        return true
+    }
+    else{
+        return num2
+    }
+}
 
-// function falsyOrTruthy(elem1, elem2) {
-    // return (!elem1) ? elem1 : elem2;
-// }
+console.log(filterOutFalsy(0,500))
 
-// console.log(falsyOrTruthy(0,5));
+
+
 
 
 
@@ -36,11 +43,7 @@
 
 // arrLength([]) --> 0
 
-// function arrLength(arr) {
-    // return arr.length;
-// }
 
-// console.log(arrLength([1, 2, 3, 4]));
 
 
 
@@ -64,20 +67,12 @@
 
 // lastElem([null,5,false]) --> false
 
-// function arrLength(arr) {
-    // return arr[arr.length - 1];
-// }
 
-// console.log(arrLength([1, 2, 3, 4]));
+let lastElem = [3,2,0,6,2]
+
+console.log(lastElem[lastElem.length - 1])
 
 
-/*To get the last element of an array you need to do length - 1 */
-
-/*Array length is how many elements there are in an array */
-
-/*remember when you are targeting an element you need to target it
-by the index number
-*/
 
 
 
@@ -101,19 +96,7 @@ by the index number
 
 // arrSum ([0,-5,-10]) --> -15
 
-// function arrSum(arr) {
-    // let sum = 0;
-    // for(let i = 0; i < arr.length; ++i) {
-        // This is how you loop through every element in an array.
-        // sum = sum + arr[i];
-        // console.log(arr[i]);
-        // console.log('run this code')
-    // }
-    // return sum;
-    // console.log('for loop broke')
-// }
 
-// console.log(arrSum([1,2,3,4,5,6]));
 
 
 /*Come back to this question and do this yourself */
@@ -149,15 +132,7 @@ by the index number
 
 
 
-// function progressiveSum(num) {
-// let sum = 0;
-// for(let i = 1; i <= num; ++i) {
-    // sum = sum + i;
-// }
-// return sum;
-// }
 
-// console.log(progressiveSum(3));
 
 
 
@@ -189,17 +164,7 @@ by the index number
 // calcTime(300) --> '05:00'
 
 
-function calcTime(seconds) {
-    let timerMinutes = Math.floor(seconds / 60);
-    let timerSeconds = seconds % 60;
 
-    if(timerMinutes.toString().length === 1) {
-        timerMinutes = '0' + timerMinutes
-    }
-    return timerMinutes + ':' + timerSeconds;
-}
-
-console.log(calcTime(500));
 
 
 
@@ -225,17 +190,7 @@ console.log(calcTime(500));
 
 // getMax([-300,-100,-200]) --> -100
 
-function getMax(arr) {
-    let max =arr[0];
-for(let i = 0; i < arr.length; ++i){
-if (arr[i] > max) {
-    max = arr[i];
-    }
-  }
-  return max;
-}
 
-console.log(getMax([1000, 3, 500]));
 
 /*Attempt this problem multiple times */
 /*And solve it yourself */
@@ -274,16 +229,7 @@ console.log(getMax([1000, 3, 500]));
 // - Use the array reverse property
 
 
-function reverseString(str) {
-    let reversedString = '';
-for(let i = 0; i < str.length; ++i) {
-    // This is how you loop through every character in a string
-    reversedString = str[i] + reversedString;
-    }
-    return reversedString;
-  }
 
-console.log(reverseString('abc'));
 
 
 
@@ -313,13 +259,7 @@ console.log(reverseString('abc'));
 // - Array 'map'
 
 
-function convertToZeros(arr) {
-    return arr.map(elem =>{
-        return 5
-    });
-}
 
-console.log(convertToZeros([1,2,3,4,5]))
 
 
 
@@ -354,13 +294,6 @@ console.log(convertToZeros([1,2,3,4,5]))
 
 
 
-function removeApples(arr) {
-    return arr.filter(elem => {
-        return elem ==='Apple'
-    })
-}
-
-console.log(removeApples(['Banana' , 'Apple' , 'Orange' , 'Apple']));
 
 
 
@@ -394,17 +327,7 @@ console.log(removeApples(['Banana' , 'Apple' , 'Orange' , 'Apple']));
 // - Solution 2: Array 'filter'
 
 
-function filterOutFalsy(arr) {
-    let truthyArr = [];
-    for (let i = 0; i < arr.length; ++i) {
-        if (!!arr[i] === true) {
-            truthyArr.push(arr[i]);
-        }
-    }
-    return truthyArr;
-}
 
-console.log(filterOutFalsy(['', NaN, 500, null, false, 0]))
 
 
 /* Attempt this problem yourself over and over */
@@ -437,11 +360,7 @@ console.log(filterOutFalsy(['', NaN, 500, null, false, 0]))
 
 
 
-function convertToBoolean(arr) {
-    return arr.map(elem => !!elem)
-}
 
-console.log(convertToBoolean(['', NaN, false, null, false, 0]));
 
 /*Go through these questions over and over again */
 
